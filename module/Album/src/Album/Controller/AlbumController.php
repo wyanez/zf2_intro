21e20a68e305a39295633da8ee892fa1619d7f88
William Yanez <wyanez@gmail.com>
2013-04-27 03:07:57 -0400

    Creado e inicializado el Modulo Album: controller, view index y router

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController{
+	public function indexAction(){
+		//return new ViewModel();
+	}
+}
+?>
